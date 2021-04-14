d6b84c936e6dd931f67bd40eb8c7de730e90ef2d
wangfei <5810014@qq.com>
2021-04-14 20:52:59 +0800

    1

--- bbs/src/main/java/com/saybiu/controller/UserController.java
+++ bbs/src/main/java/com/saybiu/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.saybiu.controller;
 
+import com.saybiu.exception.ServiceException;
+import com.saybiu.response.CommonCode;
 import com.saybiu.service.UserService;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
@@ -7,16 +9,37 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class UserController {
-    @Resource
-    private UserService userService;
-    @RequestMapping
+@RequestMapping(value="test1")
+@ResponseBody
+    public Map test1()
+{
+    Map map=new HashMap<>();
+    map.put("data",123);
+    return map;
+}
+    @RequestMapping(value="test2")
     @ResponseBody
-    public void printHash(String name)
+    public String test2()
     {
-        userService.setName(name);
-        userService.printHash();
+
+        return "123";
     }
+    @RequestMapping(value="test3")
+    @ResponseBody
+    public void test3()
+    {
+
+    }
+    @RequestMapping(value="test4")
+    @ResponseBody
+    public void test4()
+    {
+        throw new ServiceException(CommonCode.SERVER_ERROR);
+    }
+
 }
